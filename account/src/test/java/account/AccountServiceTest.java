ddfd54e65df256877392932a38e84ee80e04516d
xiao <854609899@qq.com>
2020-11-10 16:10:52 +0800

    添加jacoco测试覆盖率

---
+++ account/src/test/java/account/AccountServiceTest.java
@@ -0,0 +1,19 @@
+package account;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Author Administrator
+ * Date 2020/11/10 0010 下午 3:56
+ * Description AccountService测试
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class AccountServiceTest {
+
+    @Test
+    public void test1(){
+        System.out.println("GG");
+    }
+}
